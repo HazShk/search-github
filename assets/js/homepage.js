f27bb3de4e89a48204aeaa4e62d6e3378a947edb
Hassan Shaikh <shaikh.hassan01@outlook.com>
2025-01-02 15:51:04 -0500

    Created containers in HTML and a function to display the repo data inside the container

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -21,10 +23,59 @@ var getUserRepos = function (user) {
   //make the call
   fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
-      console.log(data);
+      //console.log(data);
+      displayRepos(data, user);
     });
   });
 };
 
+var displayRepos = function (repos, searchTerm) {
+  console.log(repos);
+  //console.log(searchTerm);
+
+  //clear old content before displaying data
+  repoContainerEl.textContent = "";
+
+  //set user name for searchterm
+  repoSearchTerm.textContent = searchTerm;
+
+  //display data, iterate over repos
+  for (var i = 0; i < repos.length; i++) {
+    //format repo name
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
+    // create a container for each repo
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+    // create a span element to hold repository name
+    var titleEl = document.createElement("span");
+    titleEl.textContent = repoName;
+
+    // append to container
+    repoEl.appendChild(titleEl);
+
+    //create issues (status) element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    //check if repo has issues
+    if (repos[i].open_issues_count > 0) {
+      statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        repos[i].open_issues_count +
+        " issue(s)";
+    } else {
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+    // append to container
+    repoEl.appendChild(statusEl);
+
+    // append container to the dom
+    repoContainerEl.appendChild(repoEl);
+  }
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
-//getUserRepos("facebook");
+//getUserRepos("microsoft");
