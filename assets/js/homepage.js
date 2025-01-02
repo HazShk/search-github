cf4c2577c49f03e73e4a85946fe5faf0eac4c857
Hassan Shaikh <shaikh.hassan01@outlook.com>
2025-01-01 20:51:04 -0500

    used DOM to capture the form input value and create the form submit handler function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,21 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  //console.log(event);
+  //get username value
+  var userName = nameInputEl.value.trim();
+  console.log(userName);
+  if (userName) {
+    getUserRepos(userName);
+    //clear the value for next search
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a valid github username");
+  }
+};
+
 var getUserRepos = function (user) {
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
   //make the call
@@ -7,4 +25,6 @@ var getUserRepos = function (user) {
     });
   });
 };
-getUserRepos("facebook");
+
+userFormEl.addEventListener("submit", formSubmitHandler);
+//getUserRepos("facebook");
